be409b047dbbd3235d06a2fd0ffad6ec55f0a5d6
Neogiciaa <chevalier.stepheh@hotmail.com>
2025-01-29 14:18:53 +0100

    Added userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,10 @@
 import express from "express";
+import { editUser, getUsers, removeUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
-router.get('/getAllUsers', getAllUsers);
+router.get('/users', getUsers);
+router.put('/users/:id', editUser);
+router.delete('/users/:id', removeUser);
+
+export default router;
