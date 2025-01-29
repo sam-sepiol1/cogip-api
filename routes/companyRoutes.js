4c6d645cdc8608005cbf5c0e1ad9c42bf6e33184
MohamedB <mohamed.berrabah11@gmail.com>
2025-01-29 16:08:05 +0100

    Added table companies CRUD Routes

---
+++ routes/companyRoutes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import {getCompanies, deleteCompany, updateCompanies, createCompanies} from "../controllers/companyController.js";
+
+
+const router = express.Router();
+
+
+router.get('/company', getCompanies);
+router.put('/company/:id', updateCompanies);
+router.post('/company', createCompanies);
+router.delete('/company/:id', deleteCompany);
+
+export default router;
