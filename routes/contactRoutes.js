4a3fdbaf1b8baeeb3e482b21f15f082408eae4f8
Neogiciaa <chevalier.stephen@hotmail.com>
2025-01-30 11:45:39 +0100

    Added contact CRUD

---
+++ routes/contactRoutes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import {
+    saveContact,
+    fetchContacts,
+    updateContact,
+    removeContact,
+} from "../controllers/contactController.js";
+
+const router = express.Router();
+
+router.get('/contact', fetchContacts);
+router.put('/contact/:id', updateContact);
+router.post('/contact', saveContact);
+router.delete('/contact/:id', removeContact);
+
+export default router;
