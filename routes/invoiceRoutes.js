4c8fed016116ad87efb8665ce9407433f877c496
MohamedB <mohamed.berrabah11@gmail.com>
2025-01-30 11:06:14 +0100

    Added invoice crud Routes

--- routes/invoiceRoutes.js
+++ routes/invoiceRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import {createOneInvoice, deleteInvoice, getAllInvoices, updateOneInvoice} from "../controllers/invoiceController.js";
+import { createOneInvoice, deleteInvoice, getAllInvoices, updateOneInvoice, getOneInvoice } from "../controllers/invoiceController.js";
 
 const router = express.Router();
 
 router.get('/invoice', getAllInvoices );
+router.get('/invoice/:id', getOneInvoice);
 router.put('/invoice/:id', updateOneInvoice );
 router.post('/invoice', createOneInvoice );
 router.delete('/invoice/:id', deleteInvoice );
