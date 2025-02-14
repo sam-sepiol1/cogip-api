515881da973721782323475184ca1d72fc888f86
Sam Sepiol <94198807+sam-sepiol1@users.noreply.github.com>
2025-02-14 16:09:36 +0100

    add route that check user authentication

--- routes/authentificationRoutes.js
+++ routes/authentificationRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createUser, logUser } from "../controllers/authentificationController.js";
+import { createUser, logUser, checkAuthStatus } from "../controllers/authentificationController.js";
 
 const router = express.Router();
 
 router.post('/auth/register', createUser);
 router.post('/auth/login', logUser);
+router.get('/auth/check', checkAuthStatus);
 
 export default router;
