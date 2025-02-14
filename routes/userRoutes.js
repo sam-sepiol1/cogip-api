dc3dc3dfae45bf25abc214fc604e95fa5d615345
Sam Sepiol <94198807+sam-sepiol1@users.noreply.github.com>
2025-02-14 16:09:14 +0100

    add route to get user by id

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { editUser, getUsers, removeUser } from "../controllers/userController.js";
+import { editUser, getUsers, getUser, removeUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.get('/users', getUsers);
+router.get('/users/:id', getUser);
 router.patch('/users/:id', editUser);
 router.delete('/users/:id', removeUser);
 
